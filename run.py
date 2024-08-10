977161e1f0dd0e52d8386b64271c4f62bc54f3d1
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-08-10 22:55:04 +0000

    add form to contact us page

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -39,8 +39,11 @@ def about_member(member_name):
 
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method =="POST":
+        # print(request.form)
+        return request.form
     return render_template('contact.html', page_title='Contact')
 
 
