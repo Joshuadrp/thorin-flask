8f563f1093abb69e5088da6dd0c904f553e5fa72
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-08-10 22:37:12 +0000

    add advanced routing in the about page for the members

--- run.py
+++ run.py
@@ -17,6 +17,28 @@ def about():
     return render_template('about.html', page_title='About', company=data)
 
 
+@app.route('/about/<member_name>')
+def about_member(member_name):
+    member = None
+
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        
+        # Find the member with the matching `url`
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+                break  # Break the loop once the member is found
+
+    # Handle the case where the member is not found
+    if member is None:
+        return abort(404, description="Member not found")
+    
+    # Return the member's name as an HTML response
+    return render_template('member.html', member=member)
+
+
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html', page_title='Contact')
